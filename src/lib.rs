37631e577e25ea782f0595e63e24a113f86295c3
Scott Hutton <schutton@cisco.com>
2024-03-06 11:17:14 -0800

    Refine error handling and destination management

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,32 @@
-mod entity;
 mod route_entry;
 mod routing_flag;
 
 // Exports
-pub use entity::Entity;
 pub use route_entry::RouteEntry;
 pub use routing_flag::RoutingFlag;
 
-use anyhow::{anyhow, Result};
-use std::{collections::HashMap, net::IpAddr};
+use cidr::AnyIpCidr;
+use mac_address::MacAddress;
+use std::{collections::HashMap, net::IpAddr, process::ExitStatus, string::FromUtf8Error};
 use tokio::process::Command;
 
 const NETSTAT_PATH: &str = "/usr/sbin/netstat";
+
+/// A generic network entity
+#[derive(Debug, Clone)]
+pub enum Entity {
+    Default,
+    Cidr(AnyIpCidr),
+    Link(String),
+    Mac(MacAddress),
+}
+
+/// A destination entity with an optional zone
+#[derive(Clone, Debug)]
+pub struct Destination {
+    pub entity: crate::Entity,
+    pub zone: Option<String>,
+}
 
 /// A snapshot of the routing table
 #[derive(Debug)]
@@ -19,6 +34,27 @@ pub struct RoutingTable {
     routes: Vec<RouteEntry>,
     /// Map of interfaces to their default routers
     if_router: HashMap<String, Vec<IpAddr>>,
+}
+
+/// Various errors
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("failed to execute {NETSTAT_PATH}: {0}")]
+    NetstatExec(std::io::Error),
+    #[error("failed to get routing table: {0}")]
+    NetstatFail(ExitStatus),
+    #[error("netstat output not non-UTF-8")]
+    NetstatUtf8(FromUtf8Error),
+    #[error("Unrecognized section name {0:?}")]
+    NetstatParseUnrecognizedSectionName(String),
+    #[error("{0:?} section missing headers")]
+    NetstatParseNoHeaders(String),
+    #[error("parsing route entry: {0}")]
+    RouteEntryParse(#[from] route_entry::Error),
+    #[error("CIDR first address neither V4 nor V6")]
+    CidrNotV4V6,
+    #[error("route entry found before protocol (Internet/Internet6) found.")]
+    EntryBeforeProto,
 }
 
 /// Internet Protocols associated with routing table entries
@@ -35,7 +71,7 @@ impl RoutingTable {
     ///
     /// Returns an error if the `netstat` command fails to execute, or returns
     /// unparseable output.
-    pub async fn load_from_netstat() -> Result<Self> {
+    pub async fn load_from_netstat() -> Result<Self, Error> {
         let output = query_netstat_routing_table().await?;
         Self::from_netstat_output(&output)
     }
@@ -46,7 +82,7 @@ impl RoutingTable {
     /// # Errors
     ///
     /// Returns an error
-    pub fn from_netstat_output(output: &str) -> Result<RoutingTable> {
+    pub fn from_netstat_output(output: &str) -> Result<RoutingTable, Error> {
         let mut lines = output.lines();
         let mut headers = vec![];
         let mut routes = vec![];
@@ -62,34 +98,33 @@ impl RoutingTable {
                     proto = match section {
                         "Internet:" => Some(Protocol::V4),
                         "Internet6:" => Some(Protocol::V6),
-                        _ => return Err(anyhow!("Unrecognized section name {proto:?}")),
+                        _ => {
+                            return Err(Error::NetstatParseUnrecognizedSectionName(section.into()))
+                        }
                     };
                     // Next line will contain the column headers
                     if let Some(line) = lines.next() {
                         headers = line.split_ascii_whitespace().collect();
                     } else {
-                        return Err(anyhow!("No headers found after {:?} section", section));
+                        return Err(Error::NetstatParseNoHeaders(section.into()));
                     }
                     continue;
                 }
                 entry => {
                     if let Some(proto) = proto {
                         let route = RouteEntry::parse(proto, entry, &headers)?;
-                        if let (Entity::Default, Entity::Cidr(cidr)) = (&route.dest, &route.gateway)
+                        if let (Entity::Default, Entity::Cidr(cidr)) =
+                            (&route.dest.entity, &route.gateway.entity)
                         {
                             if cidr.is_host_address() {
                                 let route = route.clone();
                                 let gws = if_router.entry(route.net_if).or_insert_with(Vec::new);
-                                gws.push(cidr.first_address().ok_or_else(|| {
-                                    anyhow!("CIDR first address neither V4 nor V6")
-                                })?);
+                                gws.push(cidr.first_address().ok_or(Error::CidrNotV4V6)?);
                             }
                         }
                         routes.push(route);
                     } else {
-                        return Err(anyhow!(
-                            "route entry found before protocol (Internet/Internet6) found."
-                        ));
+                        return Err(Error::EntryBeforeProto);
                     }
                 }
             };
@@ -101,6 +136,8 @@ impl RoutingTable {
     /// address.
     #[must_use]
     pub fn find_route_entry(&self, addr: IpAddr) -> Option<&RouteEntry> {
+        // TODO: implement a proper lookup table and/or short-circuit on an
+        // exact match
         self.routes
             .iter()
             .filter(|route| route.contains(addr))
@@ -116,15 +153,15 @@ impl RoutingTable {
     }
 }
 
-async fn query_netstat_routing_table() -> Result<String> {
+async fn query_netstat_routing_table() -> Result<String, Error> {
     let output = Command::new(NETSTAT_PATH)
         .arg("-rn")
         .stdin(std::process::Stdio::null())
         .output()
-        .await?;
+        .await
+        .map_err(Error::NetstatExec)?;
     if !output.status.success() {
-        return Err(anyhow!("Failed to get routing table: {}", output.status));
+        return Err(Error::NetstatFail(output.status));
     }
-    String::from_utf8(output.stdout)
-        .map_err(|e| anyhow!("netstat output failed UTF-8 conversion: {}", e))
+    String::from_utf8(output.stdout).map_err(Error::NetstatUtf8)
 }
--- src/route_entry.rs
+++ src/route_entry.rs
@@ -1,5 +1,4 @@
-use crate::{Entity, Protocol, RoutingFlag};
-use anyhow::{anyhow, Result};
+use crate::{Destination, Entity, Protocol, RoutingFlag};
 use cidr::AnyIpCidr;
 use mac_address::MacAddress;
 use std::{
@@ -15,16 +14,10 @@ pub struct RouteEntry {
     pub proto: Protocol,
 
     /// Destination.  E.g., a host or CIDR
-    pub dest: Entity,
-
-    /// Destination's zone qualifer (primarily for IPv6)
-    pub dest_zone: Option<String>,
+    pub dest: Destination,
 
     /// Gateway (i.e., how to reach the destination)
-    pub gateway: Entity,
-
-    /// Gateway's zone (primarily for IPv6)
-    pub gw_zone: Option<String>,
+    pub gateway: Destination,
 
     /// Routing flags
     pub flags: HashSet<RoutingFlag>,
@@ -36,47 +29,84 @@ pub struct RouteEntry {
     pub expires: Option<Duration>,
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("parsing destination CIDR {value:?}: {err}")]
+    ParseDestination {
+        value: String,
+        err: cidr::errors::NetworkParseError,
+    },
+
+    #[error("parsing MAC addr {dest:?}: {err}")]
+    ParseMacAddr {
+        dest: String,
+        err: mac_address::MacParseError,
+    },
+
+    #[error("unparseable byte in IPv4 address {addr:?}: {err}")]
+    ParseIPv4AddrBadInt {
+        addr: String,
+        err: std::num::ParseIntError,
+    },
+
+    #[error("invalid number of IPv4 address components ({n_comps}) in {addr:?}")]
+    ParseIPv4AddrNComps { n_comps: usize, addr: String },
+
+    #[error("invalid expiration {expiration:?}: {err}")]
+    ParseExpiration {
+        expiration: String,
+        err: std::num::ParseIntError,
+    },
+
+    #[error("missing destination")]
+    MissingDestination,
+
+    #[error("missing gateway")]
+    MissingGateway,
+
+    #[error("missing network interface")]
+    MissingInterface,
+}
+
 impl RouteEntry {
-    pub(crate) fn parse(proto: Protocol, line: &str, headers: &[&str]) -> Result<Self> {
+    /// Parse a textual route entry from the netstat output, specifying the
+    /// current protocols and active column headers.
+    pub(crate) fn parse(proto: Protocol, line: &str, headers: &[&str]) -> Result<Self, Error> {
         let fields: Vec<String> = line.split_ascii_whitespace().map(str::to_string).collect();
         let mut flags = HashSet::new();
-        let mut dest_and_zone: Option<(Entity, Option<String>)> = None;
-        let mut gw_and_zone: Option<(Entity, Option<String>)> = None;
+        let mut dest = None;
+        let mut gateway = None;
         let mut net_if: Option<String> = None;
         let mut expires = None;
 
         // Scan through the fields, matching them up with the headers.
         for (header, field) in headers.iter().zip(fields) {
             match *header {
-                "Destination" => dest_and_zone = Some(parse_destination(&field)?),
-                "Gateway" => gw_and_zone = Some(parse_destination(&field)?),
+                "Destination" => dest = Some(parse_destination(&field)?),
+                "Gateway" => gateway = Some(parse_destination(&field)?),
                 "Flags" => flags = parse_flags(&field),
                 "Netif" => net_if = Some(field),
                 "Expire" => expires = parse_expire(&field)?,
                 _ => (),
             }
         }
-        let (dest, dest_zone) = dest_and_zone.ok_or_else(|| anyhow!("No destination found"))?;
-        let (gateway, gw_zone) = gw_and_zone.ok_or_else(|| anyhow!("No gateway found"))?;
-        let net_if = net_if.ok_or_else(|| anyhow!("No network interface found"))?;
 
         let route = RouteEntry {
             proto,
-            dest,
-            dest_zone,
-            gateway,
-            gw_zone,
+            dest: dest.ok_or(Error::MissingDestination)?,
+            gateway: gateway.ok_or(Error::MissingGateway)?,
             flags,
-            net_if,
+            net_if: net_if.ok_or(Error::MissingInterface)?,
             expires,
         };
         Ok(route)
     }
+
     /// Return whether the specified route's destination is appropriate for the given address
     pub(crate) fn contains(&self, addr: IpAddr) -> bool {
-        match self.dest {
+        match self.dest.entity {
             Entity::Cidr(cidr) => cidr.contains(&addr),
-            Entity::Default => match self.gateway {
+            Entity::Default => match self.gateway.entity {
                 Entity::Cidr(_) => match addr {
                     IpAddr::V4(_) => matches!(self.proto, Protocol::V4),
                     // FIXME: IPv6 should take zone into account
@@ -95,16 +125,16 @@ impl RouteEntry {
     /// it resolves to an identified device or interface, or has a larger network
     /// length
     pub(crate) fn most_precise<'a>(&'a self, other: &'a Self) -> &'a Self {
-        match self.dest {
+        match self.dest.entity {
             // If this is a hardware address, we already know it's on the same local network, and it's in the ARP table
             Entity::Mac(_) => self,
-            Entity::Link(_) => match other.dest {
+            Entity::Link(_) => match other.dest.entity {
                 // The other specifies a hardware address -- it's better
                 Entity::Mac(_) => other,
                 // Otherwise, just default to the LHS
                 _ => self,
             },
-            Entity::Cidr(cidr) => match other.dest {
+            Entity::Cidr(cidr) => match other.dest.entity {
                 Entity::Mac(_) | Entity::Link(_) => other,
                 Entity::Cidr(other_cidr) => {
                     if let Some(cidr_nl) = cidr.network_length() {
@@ -123,7 +153,7 @@ impl RouteEntry {
                 }
                 Entity::Default => self,
             },
-            Entity::Default => match other.dest {
+            Entity::Default => match other.dest.entity {
                 Entity::Default => self,
                 _ => other,
             },
@@ -131,42 +161,69 @@ impl RouteEntry {
     }
 }
 
-fn parse_destination(dest: &str) -> Result<(Entity, Option<String>)> {
+fn parse_destination(dest: &str) -> Result<Destination, Error> {
     if dest.starts_with("link") {
-        return Ok((Entity::Link(dest.to_owned()), None));
+        return Ok(Destination {
+            entity: Entity::Link(dest.to_owned()),
+            zone: None,
+        });
     }
     Ok(if let Some(pos) = dest.find('%') {
         // This route contains a zone ID
         // See: https://superuser.com/questions/99746/why-is-there-a-percent-sign-in-the-ipv6-address
         let (addr, zone_etc) = dest.split_at(pos);
-        let addr: AnyIpCidr = addr.parse()?;
+        let addr: AnyIpCidr = addr.parse().map_err(|err| Error::ParseDestination {
+            value: addr.into(),
+            err,
+        })?;
         let mut zone_etc = zone_etc.split('/');
         let zone = zone_etc.next().map(ToOwned::to_owned);
 
         if let Some(bits) = zone_etc.next() {
             // Just reassemble it without the %zone and run it through the regular parser
             let s = format!("{addr}{bits}");
-            (parse_simple_destination(&s)?, zone)
+            Destination {
+                entity: parse_simple_destination(&s)?,
+                zone,
+            }
         } else {
-            (Entity::Cidr(addr), zone)
+            Destination {
+                entity: Entity::Cidr(addr),
+                zone,
+            }
         }
     } else {
-        (parse_simple_destination(dest)?, None)
+        Destination {
+            entity: parse_simple_destination(dest)?,
+            zone: None,
+        }
     })
 }
 
-fn parse_simple_destination(dest: &str) -> Result<Entity> {
+fn parse_simple_destination(dest: &str) -> Result<Entity, Error> {
     Ok(match dest {
         "default" => Entity::Default,
 
-        cidr if cidr.contains('/') => Entity::Cidr(cidr.parse()?),
+        cidr if cidr.contains('/') => {
+            Entity::Cidr(cidr.parse().map_err(|err| Error::ParseDestination {
+                value: cidr.into(),
+                err,
+            })?)
+        }
         // IPv4 host
         addr if addr.contains('.') => {
             if let Ok(ipv4addr) = parse_ipv4dest(addr) {
                 Entity::Cidr(AnyIpCidr::new_host(IpAddr::V4(ipv4addr)))
             } else {
                 // Bridge broadcast addresses sometimes contain a dot-delimited MAC address
-                Entity::Mac(addr.replace('.', ":").parse::<MacAddress>()?)
+                Entity::Mac(
+                    addr.replace('.', ":")
+                        .parse::<MacAddress>()
+                        .map_err(|err| Error::ParseMacAddr {
+                            dest: addr.into(),
+                            err,
+                        })?,
+                )
             }
         }
         // IPv6 host
@@ -175,7 +232,13 @@ fn parse_simple_destination(dest: &str) -> Result<Entity> {
                 Entity::Cidr(AnyIpCidr::new_host(IpAddr::V6(v6addr)))
             } else {
                 // Try as a MAC address
-                Entity::Mac(addr.parse::<MacAddress>()?)
+                Entity::Mac(
+                    addr.parse::<MacAddress>()
+                        .map_err(|err| Error::ParseMacAddr {
+                            dest: addr.into(),
+                            err,
+                        })?,
+                )
             }
         }
         // Match bare numbers
@@ -187,31 +250,37 @@ fn parse_flags(flags_s: &str) -> HashSet<RoutingFlag> {
     flags_s.chars().map(RoutingFlag::from).collect()
 }
 
-fn parse_expire(s: &str) -> Result<Option<Duration>> {
+fn parse_expire(s: &str) -> Result<Option<Duration>, Error> {
     match s {
         "!" => Ok(None),
-        n => Ok(Some(Duration::from_secs(n.parse()?))),
+        n => Ok(Some(Duration::from_secs(n.parse().map_err(|err| {
+            Error::ParseExpiration {
+                expiration: s.into(),
+                err,
+            }
+        })?))),
     }
 }
 
-fn parse_ipv4dest(dest: &str) -> Result<Ipv4Addr> {
-    dest.parse::<Ipv4Addr>()
-        .or_else(|_| {
-            let parts: Vec<u8> = dest
-                .split('.')
-                .map(str::parse)
-                .collect::<std::result::Result<Vec<u8>, std::num::ParseIntError>>()?;
-            // This bizarre byte-ordering comes from inet_addr(3)
-            match parts.len() {
-                3 => Ok(Ipv4Addr::new(parts[0], parts[1], 0, parts[2])),
-                2 => Ok(Ipv4Addr::new(parts[0], 0, 0, parts[1])),
-                1 => Ok(Ipv4Addr::new(0, 0, 0, parts[0])),
-                len => Err(anyhow!(
-                    "Invalid number of IPv4 address components ({}) in {:?}",
-                    len,
-                    dest
-                )),
-            }
-        })
-        .map_err(|e| anyhow!("Unable to parse {:?} as IpAddr: {}", dest, e))
+fn parse_ipv4dest(dest: &str) -> Result<Ipv4Addr, Error> {
+    dest.parse::<Ipv4Addr>().or_else(|_| {
+        let parts: Vec<u8> = dest
+            .split('.')
+            .map(str::parse)
+            .collect::<std::result::Result<Vec<u8>, std::num::ParseIntError>>()
+            .map_err(|err| Error::ParseIPv4AddrBadInt {
+                addr: dest.into(),
+                err,
+            })?;
+        // This bizarre byte-ordering comes from inet_addr(3)
+        match parts.len() {
+            3 => Ok(Ipv4Addr::new(parts[0], parts[1], 0, parts[2])),
+            2 => Ok(Ipv4Addr::new(parts[0], 0, 0, parts[1])),
+            1 => Ok(Ipv4Addr::new(0, 0, 0, parts[0])),
+            len => Err(Error::ParseIPv4AddrNComps {
+                n_comps: len,
+                addr: dest.into(),
+            }),
+        }
+    })
 }
